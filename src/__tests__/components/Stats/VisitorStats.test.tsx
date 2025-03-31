fb5a5fd0c4b5f494c02ec001ee423b4bc49e2dcd
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2025-03-31 13:44:06 -0300

    fix: fixed unused var on test

--- src/__tests__/components/Stats/VisitorStats.test.tsx
+++ src/__tests__/components/Stats/VisitorStats.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import VisitorStats from '@components/Stats/VisitorStats';
 
 // Simula userAgent e referrer
