a92575c36ba9cff3049550919c5d48cfd9ce21b7
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2025-04-01 08:39:23 -0300

    fix(seo): types of repo adjustments

--- src/components/Portfolio/ProjectList.tsx
+++ src/components/Portfolio/ProjectList.tsx
@@ -6,11 +6,19 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+interface GitHubRepo {
+  id: number;
+  html_url: string;
+  name: string;
+  description: string | null;
+  stargazers_count: number;
+}
+
 const GITHUB_USERNAME = 'narradorww';
 const REPOS_TO_DISPLAY = 8;
 
 const ProjectList = () => {
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState<GitHubRepo[]>([]);
 
   useEffect(() => {
     const fetchRepos = async () => {
@@ -18,7 +26,7 @@ const ProjectList = () => {
         const response = await fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`);
         const data = await response.json();
         const sortedRepos = data
-          .sort((a, b) => b.stargazers_count - a.stargazers_count)
+          .sort((a: { stargazers_count: number; }, b: { stargazers_count: number; }) => b.stargazers_count - a.stargazers_count)
           .slice(0, REPOS_TO_DISPLAY);
         setRepos(sortedRepos);
       } catch (error) {
@@ -55,7 +63,7 @@ const ProjectList = () => {
     <div>
       <h2>Principais Projetos</h2>
       <Slider {...settings}>
-        {repos.map((repo) => (
+        {repos.map((repo: GitHubRepo) => (
           <div key={repo.id}>
             <h3>
               <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
