90b268a233f7b3437f1dd558ecd004005d3e4a6d
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2025-04-21 18:18:22 -0300

    fixe type on mongodb lib

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI as string;
 const options = {};
@@ -6,16 +6,22 @@ const options = {};
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!uri) {
+// 👇 Tipagem customizada para o Node.js global
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
-if (process.env.NODE_ENV === "development") {
-  if (!(global as any)._mongoClientPromise) {
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
